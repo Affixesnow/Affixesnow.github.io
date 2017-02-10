135b119c60e5f8201133769459d7bd5c20122a5f
Vilde Helmerson <vildehelmerson@gmail.com>
2017-02-11 00:37:48 +0100

    changed week no.

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ jQuery(document).ready(function($) {
   $(function(){
     var today = new Date();
     var weekno = today.getWeek();
-    var affixWeekEU = weekno + 4;
-    var affixWeekUS = weekno + 2;
+    var affixWeekEU = weekno + 3;
+    var affixWeekUS = weekno + 1;
 
     if(new Date().getDay() >= 3){
       affixWeekEU += 1;
